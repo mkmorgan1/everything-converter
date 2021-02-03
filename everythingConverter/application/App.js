19e19ae21927123ac27b956c978f72e4c3b52530
Matthew <m_kmorgan@hotmail.com>
2021-02-02 20:35:50 -0700

    created a couple buttons

--- everythingConverter/application/App.js
+++ everythingConverter/application/App.js
@@ -8,21 +8,19 @@
 
 import React from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
+  SafeAreaView, ScrollView, View,
+  Text, StatusBar, Button, Alert,
+  TouchableOpacity
 } from 'react-native';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import { LearnMoreLinks } from 'react-native/Libraries/NewAppScreen';
+
+// import styles from './styles.module.css';
+import styles from './styles.js';
+
+const redirect = (e) => {
+  Alert.alert('redirect is not set up yet');
+}
 
 const App: () => React$Node = () => {
   return (
@@ -32,36 +30,34 @@ const App: () => React$Node = () => {
         <ScrollView
           contentInsetAdjustmentBehavior="automatic"
           style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
+          {/* {global.HermesInternal == null ? null : (
             <View style={styles.engine}>
               <Text style={styles.footer}>Engine: Hermes</Text>
             </View>
-          )}
+          )} */}
           <View style={styles.body}>
             <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
+              <Text style={styles.sectionTitle}>Everything Converter</Text>
+              {/* TIME ZONE */}
+              <TouchableOpacity style={styles.button} onPress={redirect}>
+                <Text style={styles.buttonText}>Time Zone</Text>
+              </TouchableOpacity>
+
+              {/* LENGTH */}
+              <TouchableOpacity style={styles.button} onPress={redirect}>
+                <Text style={styles.buttonText}>Length</Text>
+              </TouchableOpacity>
+
+              {/* WEIGHT */}
+              <TouchableOpacity style={styles.button} onPress={redirect}>
+                <Text style={styles.buttonText}>Weight</Text>
+              </TouchableOpacity>
+
             </View>
             <View style={styles.sectionContainer}>
               <Text style={styles.sectionTitle}>Learn More</Text>
               <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
+                Cmd + d = debug | Cmd + r = reload
               </Text>
             </View>
             <LearnMoreLinks />
@@ -71,44 +67,5 @@ const App: () => React$Node = () => {
     </>
   );
 };
-
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
-  },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
-  },
-});
 
 export default App;
---
+++ everythingConverter/application/styles.js
@@ -0,0 +1,60 @@
+import { StyleSheet } from 'react-native';
+import { Colors } from 'react-native/Libraries/NewAppScreen';
+
+const styles = StyleSheet.create({
+  scrollView: {
+    backgroundColor: '#FCDCDF',
+  },
+  engine: {
+    position: 'absolute',
+    right: 0,
+  },
+  body: {
+    backgroundColor: 'white',
+  },
+  sectionContainer: {
+    marginTop: 32,
+    paddingHorizontal: 24,
+  },
+  sectionTitle: {
+    fontSize: 30,
+    fontWeight: '600',
+    color: 'black',
+    textAlign: 'center',
+    marginBottom: 20,
+  },
+  sectionDescription: {
+    marginTop: 8,
+    fontSize: 18,
+    fontWeight: '400',
+    color: 'grey',
+  },
+  highlight: {
+    fontWeight: '700',
+  },
+  footer: {
+    color: 'grey',
+    fontSize: 12,
+    fontWeight: '600',
+    padding: 4,
+    paddingRight: 12,
+    textAlign: 'right',
+  },
+  button: {
+    backgroundColor: '#83D9DC',
+    color: '#FCDCDF',
+    borderRadius: 15,
+    alignItems: 'center',
+    padding: 20,
+    margin: 10,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.3,
+    shadowRadius: 1,
+  },
+  buttonText: {
+    fontSize: 20,
+  }
+});
+
+export default styles;
